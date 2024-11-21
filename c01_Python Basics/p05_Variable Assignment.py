3058c2203bc47a92e65cb81d3e46822b5db039e5
lmpescobar <lmpes@tutanota.com>
2024-11-21 16:29:56 -0400

    Variable Assignment

---
+++ c01_Python Basics/p05_Variable Assignment.py
@@ -0,0 +1,24 @@
+# Ejercicio
+# Asignación de variables
+
+# Descripción del ejercicio:
+# En Python, una variable permite asignar un valor a un nombre.
+# Para crear una variable `x` con un valor de 5, se utiliza el signo igual `=` como en el siguiente ejemplo:
+# x = 5
+# Una vez que se asigna un valor, se puede referir al nombre de la variable en lugar del valor en sí.
+# Nota: El signo igual `=` en Python significa asignación, no igualdad.
+
+# Instrucciones:
+# 1. Crea una variable llamada `savings` con el valor 100.
+# 2. Muestra el contenido de esta variable utilizando la función `print(savings)` en el script.
+
+# Código en Python:
+# Crear una variable savings
+savings = 100  # Asignamos el valor 100 a la variable savings.
+
+# Mostrar el valor de savings
+print(savings)  # Imprimimos el valor almacenado en la variable savings.
+
+# Explicación del código:
+# - La línea 9 asigna el valor entero 100 a la variable savings.
+# - La línea 12 utiliza la función print() para mostrar el valor actual de savings en la consola.
